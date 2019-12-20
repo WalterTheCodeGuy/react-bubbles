d9aa6b589379c0a827a234d752fbf0da162ea6f5
Walter Futch <walter.futch@gmail.com>
2019-12-20 13:37:13 -0500

    Updated App.js
    removed Navigation

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-import Navigation from './components/Navigation';
 import BubblePage from './components/BubblePage';
 import PrivateRoute from './utils/PrivateRoute';
 import Login from "./components/Login";
