9ce1d957441232352d067564d844ef96f5471457
Walter Futch <walter.futch@gmail.com>
2019-12-20 15:26:54 -0500

    Update PrivateRoute.js

--- client/src/utils/PrivateRoute.js
+++ client/src/utils/PrivateRoute.js
@@ -9,7 +9,7 @@ const PrivateRoute = ({ component: Component, ...rest }) => {
         return localStorage.getItem("token") ? (
           <Component {...props} />
         ) : (
-          <Redirect to="/login" />
+          <Redirect to="/" />
         );
       }}
     />
